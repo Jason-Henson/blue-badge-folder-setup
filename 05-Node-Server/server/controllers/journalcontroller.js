7dd6c3983dfb1499d83cd73c3554c07f0434cb7b
Jason-Henson <jason.a.henson@gmail.com>
2021-07-27 17:45:27 -0400

    first set of modules done

---
+++ 05-Node-Server/server/controllers/journalcontroller.js
@@ -0,0 +1,15 @@
+let express = require('express');
+const { restart } = require('nodemon');
+let router = express.Router(); 
+
+router.get('/practice', (req, res) => {
+    res.send("This is a practice route");
+});
+
+// Challenge #1 create route journal/about
+
+router.get('/about', (req, res) => {
+    res.send('This is the about route');
+});
+
+module.exports = router;
