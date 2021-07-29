3843fe193ad296cb0e7646944e07544b36433d09
Jason-Henson <jason.a.henson@gmail.com>
2021-07-29 15:22:12 -0400

    completed up mod 7.1

--- 05-Node-Server/server/app.js
+++ 05-Node-Server/server/app.js
@@ -1,21 +1,11 @@
 let express = require('express');
 const app = express();
+const sequelize = require('./db');
 let journal = require("./controllers/journalcontroller");
 
-
-// app.use("/test", function(req,res){
-//     res.send("This is a message form the test endpoint on the server")
-// })
-
-// app.use("/jason", (req, res) => {
-//     res.send("My name is Jason and I am twee years old... duh")
-// })
-
-// Have endpoint of journal/practice
-//send a response from taht end point (this is a practice route)
+sequelize.sync();
 
 app.use('/journal', journal);
-
 app.listen(3000, function() {
     console.log("App is listening on port 3000");
 });
