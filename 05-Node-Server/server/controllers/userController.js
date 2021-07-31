d9212a2161f0daf852a3976f1c32c9d55db82d72
Jason-Henson <jason.a.henson@gmail.com>
2021-07-31 16:05:33 -0400

    finished all of mod 9. Now starting 10 jwt

--- 05-Node-Server/server/controllers/userController.js
+++ 05-Node-Server/server/controllers/userController.js
@@ -1,13 +1,60 @@
+const { restart } = require('nodemon');
+
 const router = require('express').Router();
 const User = require('../db').import('../models/user.js');
 
-router.post('/create', (req, res) => {    
+/* ******************
+ *** USER CREATE ***
+******************* */ 
 
-    let userModel = { 
+router.post('/create', (req, res) => {
+
+    User.create({
         email: req.body.user.email,
-        password: req.body.user.email
-    };
-    User.create(userModel).then(res.send("This is our user create endpoint"));
+        password: req.body.user.password
+    })
+    .then(
+        function createSuccess(user) {
+            res.json({
+                user: user
+            });
+        }
+    )
+    .catch(err => res.status(500).json({ error: err }))
 });
+
+/* ******************
+ *** USER SIGNIN ***
+******************* */ 
+
+router.post('/login', (req, res) => {
+
+    User.findOne({
+        where: {
+            email: req.body.user.email
+        }
+    })
+
+        .then(function loginSuccess(user) {
+            if (user) {
+                res.status(200).json({
+                    user: user
+                })
+            } else {
+                res.status(500).json({ error: 'User does not exist.'})
+            }
+        })
+        .catch(err => res.status(500).json({ error: err }))
+}); 
+
+// router.post('/login', (req, res) => {
+//     User.findOne({ where: { email: req.body.user.email } }).then(function loginSuccess(user) {
+//         if(user){
+//             res.status(200).json({ user: user });
+//         } else {
+//             res.status(500).json('The email address you entered was not found');
+//         }  
+//     });                 
+// });
 
 module.exports = router; 
--- 05-Node-Server/server/models/user.js
+++ 05-Node-Server/server/models/user.js
@@ -5,7 +5,8 @@ module.exports = function (sequelize, DataTypes) {
     const User = sequelize.define('user', {
         email: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            unique: true
         },
         password: {
             type: DataTypes.STRING,
